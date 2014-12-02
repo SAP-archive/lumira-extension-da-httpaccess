9a7ac66b075b956c487658720bfc3fd80de5cdc9
Vamsi Alluri <vamsi.alluri@sap.com>
2014-12-01 16:33:51 -0800

    update docs

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ HTTPAccess - SAP Lumira Data Access Extension
 * Connect Lumira to Web API's that return JSON responses.
 * Requires the response to have a list of objects. Feel free to modify code to handle a different response structure.
 * Apimocker.exe utility serves JSON files present in the public folder
+
+https://github.com/lxn/walk - Copyright (c) 2010 The Walk Authors. All rights reserved.
+
+https://github.com/go-martini/martini - Copyright (c) 2013 Jeremy Saenz
--- httpaccess.go
+++ httpaccess.go
@@ -43,8 +43,10 @@ type HttpRequest struct {
 	reqBody string
 }
 
+//set this to true if you want to display stored DSInfo parameters in the Lumira document
 var debugFlag bool = false
 
+//send DSInfo parameters in the right format to Lumira in Preview Mode only
 func sendDSInfoBlock(httpRequest HttpRequest) {
 	var URIInfo string
 	var reqTypeInfo string
@@ -55,9 +57,16 @@ func sendDSInfoBlock(httpRequest HttpRequest) {
 
 	URIInfo = "URI;" + httpRequest.uri.String() + ";true;"
 	reqTypeInfo = "TYPE;" + httpRequest.reqType + ";true;"
+	
+	//multiple headers stored in one line as headername:headervalue; 
+	//these header separators are encoded before being sent here 
 	reqHeaderInfo = "HEADER;" + httpRequest.reqHeader + ";true;"
+	
+	//basic auth header will be created after base64 encoding
 	usernameInfo = "USERNAME;" + httpRequest.username + ";true"
 	passwordInfo = "PASSWORD;" + httpRequest.password + ";true"
+
+	//new line characters in the body are encoded before storing here
 	reqBodyInfo = "BODY;" + httpRequest.reqBody + ";true"
 
 	fmt.Println("beginDSInfo")
@@ -72,8 +81,12 @@ func sendDSInfoBlock(httpRequest HttpRequest) {
 	fmt.Println("endDSInfo")
 }
 
+//used in all three modes to send data to lumira
 func sendDataBlock(httpRequest HttpRequest) {
 	fmt.Println("beginData")
+
+	//read debug flag to switch between displaying DSInfo parameters or 
+	//data in the lumira document
 	if debugFlag {
 		readParams()
 	} else {
@@ -82,6 +95,7 @@ func sendDataBlock(httpRequest HttpRequest) {
 	fmt.Println("endData")
 }
 
+//retrieve and send parameters to the document instead of data
 func readParams() {
 	csvparamout := csv.NewWriter(os.Stdout)
 
@@ -98,6 +112,7 @@ func readParams() {
 	csvparamout.Flush()
 }
 
+//get data from source, convert it to csv and send to lumira
 func readData(httpRequest HttpRequest) {
 
 	uri := httpRequest.uri
@@ -107,6 +122,7 @@ func readData(httpRequest HttpRequest) {
 	password := charUnescape(httpRequest.password)
 	reqBody := charUnescape(httpRequest.reqBody)
 
+	//cookie jar, can be used if multiple calls are made to retreive data
 	cookieOptions := cookiejar.Options {
 		PublicSuffixList: publicsuffix.List,
 	}
@@ -118,7 +134,7 @@ func readData(httpRequest HttpRequest) {
 		Jar: jar,
 	}
 
-	//split headers from input text
+	//extract individual headers
 	var headers []string
 	var tokens []string
 	headers = strings.Split(reqHeader, PARAM_SEPARATOR)
@@ -131,6 +147,8 @@ func readData(httpRequest HttpRequest) {
 		tokens = strings.Split(headers[i], `:`)
 		req.Header.Add(tokens[0], tokens[1])
 	}
+
+	//compute the basic auth header
 	req.SetBasicAuth(username, password)
 
 	for _, value := range jar.Cookies(uri) {
@@ -139,18 +157,22 @@ func readData(httpRequest HttpRequest) {
 	}
 	//fmt.Println("Final Request is: ", req)
 	
+	//send request
 	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 
+	//read the body of the response
 	body, err := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
 
+	//convert the JSON response to CSV
 
-	//parsing response body to csv
 	var jsonOut []map[string]interface{}
+	//refer to http://blog.golang.org/json-and-go
 
+	//unmarshal response body to the map
 	err = json.Unmarshal(body, &jsonOut)
 	check(err)
 	//fmt.Println(jsonOut)
@@ -171,7 +193,7 @@ func readData(httpRequest HttpRequest) {
 		csvout.Write(header)
 	}
 
-	//TODO assuming subsequent JSON Objects have the same keys as the firsts
+	//TODO assuming subsequent JSON Objects have the same keys as the first object in the list
 	for index, _ := range jsonOut {
 		if true {
 			var record []string
@@ -192,10 +214,13 @@ func check(e error) {
 	}
 }
 
+// parse arguments passed to the executable in various modes
 func parseArguments(args []string) {
 	mode = ERROR
 
 	for i := 0; i < len(args); i++ {
+
+		//detect and save the current mode of operation requested
 		if strings.ToLower(args[i]) == "-mode" && i+1 < len(args) {
 			//fmt.Println("mode")
 
@@ -213,6 +238,7 @@ func parseArguments(args []string) {
 			//size, _ := strconv.ParseInt(args[i+1], 10, 64)
 			//fmt.Println(size)
 
+		// read the params if sent in EDIT and REFRESH modes
 		} else if strings.ToLower(args[i]) == "-params" && i+1 < len(args) {
 			params = args[i+1]
 			//fmt.Println(params)
@@ -221,16 +247,20 @@ func parseArguments(args []string) {
 	//fmt.Println(args)
 }
 
+//encoding characters that interfere when the SDK parses parameter values
+//Carriage Return, Line Feed, semi-colon, and double-quotes
 func charEscape(escapeStr string) string {
 	r := strings.NewReplacer("\n", "%0A", "\r", "%0D", "\"", "%22", ";", "%3B")
 	return r.Replace(escapeStr)
 }
 
+//decoding these characters before presenting the values to lumira
 func charUnescape(unescapeStr string) string {
 	r := strings.NewReplacer("%0A", "\n", "%0D", "\r", "%22", "\"", "%3B", ";")
 	return r.Replace(unescapeStr)
 }
 
+//main
 func main() {
 	args := os.Args[1:]
 	parseArguments(args)
@@ -244,6 +274,7 @@ func main() {
 	var reqHeaderValue *walk.TextEdit
 	var reqBodyValue *walk.TextEdit
 
+	// handle refresh and edit modes
 	if mode == REFRESH || mode == EDIT {
 		//fmt.Println("REFRESH OR EDIT")
 		//fmt.Println(params)
@@ -280,9 +311,12 @@ func main() {
 
 		//fmt.Println(lines)
 
+	//handle preview mode
 	} else if mode == PREVIEW {
 		//fmt.Println("PREVIEW")
 
+		//implementing the GUI interface to capture parameters from user
+		//some fields have defaults
 		MainWindow{
 			Title:   "Enter the URL:",
 			MinSize: Size{600, 400},
